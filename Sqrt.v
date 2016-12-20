f578ba2f5de41d8bca73eaa265f3d438ff5afd51
João Alpuim <alpuimj@gmail.com>
2016-12-20 12:59:56 +0800

    Clean-up on Sqrt.v.

--- Sqrt.v
+++ Sqrt.v
@@ -25,19 +25,69 @@ Definition find_eqb_some (h : option nat) : nat :=
     | _ => 0
   end.
 
-Definition lift_opt_prop {a} (f : a -> a -> Prop) (v1 : option a) (v2 : option a) : Prop :=
-  match v1,v2 with
-    | Some x, Some y => f x y
-    | _, _ => False
-  end.
-
 Definition Inv (Q R : Ptr) (s : nat) (si st : heap nat) :=
-    prod (lift_opt_prop (fun r s => Nat.le (square r) s) (find st R) (Some s))
-    (prod (lift_opt_prop (fun s q => Nat.lt s (square q)) (Some s) (find st Q)) (R <> Q)).
+  { q : nat & prod (find st Q = Some q) (
+  { r : nat & prod (find st R = Some r) (
+            prod (le (square r) s) (
+            prod (lt s (square q))
+                 (R <> Q))) } ) }.
 
 Definition Cond (Q R : Ptr) (st : heap nat) :=
   negb (Nat.eqb (find_eqb_some (find st R) + 1)
                 (find_eqb_some (find st Q))).
+
+Lemma ifSpec {a v} (cond : bool) (spec : PT v a) (wt we : WhileL v a) :
+  (if cond then Spec spec ⊑ wt else Spec spec ⊑ we) ->
+  (Spec spec ⊑ (if cond then wt else we)).
+Proof. destruct cond; intros; assumption. Qed.
+
+Hint Unfold isExecutable square.
+Hint Resolve Nat.ltb_lt Lt.lt_n_Sm_le.
+Hint Resolve le_0_n leb_iff_conv beq_nat_eq.
+
+Ltac WHILE I c := apply (whileSpec I c); goal_simpl.
+Ltac ITE c := apply (ifSpec c); remember c as b; destruct b; goal_simpl.
+
+(*** Derivation ***)
+
+Definition sqrtResult (ptr : Ptr) :
+  { c : WhileL nat nat & prod (sqrtSpec ptr ⊑ c) (isExecutable c) }.
+Proof.
+  econstructor; unfold sqrtSpec. split.
+  - READ ptr s.
+    NEW (s + 1) Q.
+    NEW 0 R.
+    WHILE (Inv Q R s) (Cond Q R); unfold Inv, Cond.
+    * repeat (eexists; split; eauto); repeat split; eauto.
+      rewrite Nat.add_comm; apply Nat.lt_lt_add_r; auto.
+    * READ Q vInQ.
+      READ R vInR.
+      ITE (s <? square (Nat.div2 (vInQ + vInR))).
+      WRITE Q (Nat.div2 (vInQ + vInR)).
+      RETURN tt.
+      repeat (eexists; split; eauto); repeat split; eauto.   
+      now apply Nat.ltb_lt.
+      WRITE R (Nat.div2 (vInQ + vInR)).
+      eapply (@writeSpec _ _ R (Nat.div2 (vInQ + vInR))); goal_simpl.
+      RETURN tt.
+      repeat (eexists; split; eauto); repeat split; eauto.
+      symmetry in Heqb; apply Lt.lt_n_Sm_le; now apply leb_iff_conv. 
+    * READ R vInR.
+      RETURN vInR.
+      auto.
+      rewrite e1 in i; rewrite e0 in i; simpl in i.
+      remember (vInR + 1 =? s3) as b; destruct b. 
+      apply beq_nat_eq in Heqb; now subst.
+      exfalso; now simpl in i.
+  - repeat (simpl; intros; split; intros);
+    destruct (v <? square (Nat.div2 (v0 + v1))); auto.
+Defined.
+
+Variable t : Ptr.
+
+Eval simpl in projT1 (sqrtResult t).
+
+(*** Verification ***)
 
 Definition sqrt (ptr : Ptr) : WhileL nat nat.
 refine (
@@ -103,64 +153,6 @@ Proof.
       now subst.
       exfalso; simpl in i; rewrite <- Heqb in i; auto.
       exfalso; assumption.
-Qed.
-
-Lemma ifSpec {a v} (cond : bool) (spec : PT v a) (wt we : WhileL v a) :
-  (if cond then Spec spec ⊑ wt else Spec spec ⊑ we) ->
-  (Spec spec ⊑ (if cond then wt else we)).
-Proof. destruct cond; intros; assumption. Qed.
-
-Definition sqrtResult (ptr : Ptr) :
-  {c : WhileL nat nat & sqrtSpec ptr ⊑ c}.
-Proof.
-  econstructor; unfold sqrtSpec.
-  READ ptr s.
-  NEW (s + 1) Q.
-  NEW 0 R.
-  apply whileSpec with (I := Inv Q R s) (c := Cond Q R); unfold Inv, Cond.
-  - refine_simpl.
-    unfold lift_opt_prop, square; simpl; rewrite findUpdate; simpl; apply le_0_n.
-    unfold square; simpl; rewrite findNUpdate.
-    unfold Nat.lt; rewrite findUpdate; simpl.
-    remember ((s + 1) * (s + 1)).
-    rewrite Nat.add_comm in Heqn1 at 1; simpl in Heqn1.
-    rewrite Nat.add_comm with (m := 1) in Heqn1; rewrite Heqn1.
-    apply Nat.le_add_r.
-    unfold not; intro H'; subst; assert (Ha : R <> R) by eauto; now apply Ha.
-    unfold not; intro H'; subst; assert (Ha : Q <> Q) by eauto; now apply Ha.
-  - READ Q vInQ.
-    remember (find s0 Q) as o.
-    destruct o; [ eauto | exfalso; assumption ].
-    READ R vInR.
-    unfold lift_opt_prop in l.
-    remember (find s0 R) as r; destruct r; [ eauto | exfalso; assumption ].
-    remember (s <? square (Nat.div2 (vInQ + vInR))) as b.
-    apply ifSpec with (cond := s <? square (Nat.div2 (vInQ + vInR))).
-    destruct b; rewrite <- Heqb; simpl.
-    (* WRITE Q (Nat.div2 (vInQ + vInR)). *)
-    eapply (@writeSpec _ _ Q (Nat.div2 (vInQ + vInR))).
-    intros; simpl in *; destruct_conjs'; eexists; apply e0.
-    RETURN tt; try rewrite findUpdate; try rewrite findNUpdate; auto.
-    rewrite e1 in y; rewrite e0 in l; symmetry in Heqb; now apply Nat.ltb_lt.
-    (* WRITE R (Nat.div2 (vInQ + vInR)). *)
-    eapply (@writeSpec _ _ R (Nat.div2 (vInQ + vInR))).
-    intros; simpl in *; destruct_conjs'; eexists; apply e.
-    RETURN tt;
-    try rewrite findUpdate; try rewrite findNUpdate; auto.
-    simpl; symmetry in Heqb; apply leb_iff_conv in Heqb;
-    now apply Lt.lt_n_Sm_le in Heqb.
-  - READ R vInR.
-    unfold lift_opt_prop in l.
-    remember (find s0 R) as r; destruct r; [ eauto | exfalso; assumption ].
-    RETURN vInR.
-    * rewrite e in l; simpl in l; auto.
-    * rewrite e in i; simpl in i.
-      remember (find s0 Q) as q; destruct q.
-      remember (vInR + 1 =? n2) as b; destruct b.
-      apply beq_nat_eq in Heqb.
-      now subst.
-      exfalso; simpl in i; rewrite <- Heqb in i; auto.
-      exfalso; assumption.  
 Qed.
 
 (* Refinement with Type *)
